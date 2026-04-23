2610d177f331a66fd5ed264a6a0c74d22d3bef25
Juan José Ordoñez Villalobos <49448784+coltonsini@users.noreply.github.com>
2026-04-23 12:31:37 -0500

    Use resetStyles.css instead of index.css
    Update frontend/src/main.tsx to remove the import of index.css and import resetStyles.css instead.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import Cartelera from './Cartelera.tsx'
+import './resetStyles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
